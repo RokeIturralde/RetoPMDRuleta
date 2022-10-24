a7156e0616b0c02afdc6c91a8bd7f1d3022c2224
2dam <you@example.com>
2022-10-24 13:05:24 +0200

    pantalla de fin de partida empezada

---
+++ app/src/main/java/com/example/retopmdruleta/FinDePartidaActivity.java
@@ -0,0 +1,24 @@
+package com.example.retopmdruleta;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class FinDePartidaActivity extends AppCompatActivity {
+
+    private TextView tv;
+    private Button b;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fin_de_partida);
+    }
+
+
+    public void a() {
+
+    }
+}
---
+++ app/src/main/java/com/example/retopmdruleta/PantallaConfiguracion.java
@@ -0,0 +1,43 @@
+package com.example.retopmdruleta;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+
+public class PantallaConfiguracion extends AppCompatActivity implements  RadioGroup.OnCheckedChangeListener  {
+
+
+    RadioButton radioButton = null;
+    RadioButton radioButton2 = null;
+    RadioButton radioButton3 = null;
+    RadioButton radioButton4 = null;
+    RadioButton radioButton5 = null;
+    RadioButton radioButton6 = null;
+    RadioGroup radioGroup = null;
+    Button botonCancelar = null;
+    Button botonJugar = null;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pantalla_configuracion2);
+        radioButton = (RadioButton) findViewById(R.id.radioButton);
+        radioButton2 = (RadioButton) findViewById(R.id.radioButton2);
+        radioButton3 = (RadioButton) findViewById(R.id.radioButton3);
+        radioButton4 = (RadioButton) findViewById(R.id.radioButton4);
+        radioButton5 = (RadioButton) findViewById(R.id.radioButton5);
+        radioButton6 = (RadioButton) findViewById(R.id.radioButton6);
+        radioGroup = (RadioGroup) findViewById(R.id.radioGroup);
+        radioGroup.setOnCheckedChangeListener(this);
+    }
+
+    @Override
+    public void onCheckedChanged(RadioGroup group, int checkedId) {
+
+    }
+}
