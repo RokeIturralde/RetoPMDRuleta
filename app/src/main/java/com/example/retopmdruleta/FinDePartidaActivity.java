856d756d75623febfd70ba45d045272374c151b8
2dam <you@example.com>
2022-10-24 13:39:23 +0200

    more code on FinDePartidaActivity

--- app/src/main/java/com/example/retopmdruleta/FinDePartidaActivity.java
+++ app/src/main/java/com/example/retopmdruleta/FinDePartidaActivity.java
@@ -8,13 +8,26 @@ import android.widget.TextView;
 
 public class FinDePartidaActivity extends AppCompatActivity {
 
-    private TextView tv;
     private Button b;
+    private TextView tv;
+
+    private boolean winner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fin_de_partida);
+
+        tv = (TextView) findViewById(R.id.textView);
+        b = (Button) findViewById(R.id.button);
+        b.setText(R.string.instrucciones);
+
+        if (winner)
+            tv.setText(R.string.ganado);
+        else
+            tv.setText(R.string.perdido);
+
+
     }
 
 
