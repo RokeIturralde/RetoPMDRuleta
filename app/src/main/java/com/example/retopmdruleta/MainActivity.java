2559d637bb74b7dbfdc376563d1d42b4bdcfc4eb
RokeIturralde <rokeitumendi@gmail.com>
2022-10-25 14:22:13 +0200

    almost finished with the controller

--- app/src/main/java/com/example/retopmdruleta/MainActivity.java
+++ app/src/main/java/com/example/retopmdruleta/MainActivity.java
@@ -2,13 +2,50 @@ package com.example.retopmdruleta;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button botonPropiedades = null;
+    ArrayList<String> jugadores = new ArrayList<>();
+    private TextView textView = null;
+    public static final int secondary_activity = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        botonPropiedades = (Button) findViewById(R.id.button3);
+        textView = (TextView) findViewById(R.id.textView3);
+        botonPropiedades.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Return numero de Jugadores
+                Intent intent = new Intent(MainActivity.this, PantallaConfiguracion.class);
+                // onActivityResult(1, 1, intent);
+                startActivityForResult(intent, secondary_activity);
+            }
+        });
+    }
+
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        switch (requestCode) {
+            case (secondary_activity): {
+                if (resultCode == PantallaConfiguracion.RESULT_OK) {
+                    String returnValue = data.getStringExtra("numeroJugadores");
+                    jugadores = data.getStringArrayListExtra("jugadores");
+                    textView.setText(returnValue);
+                    textView.setText(textView.getText() + " " + jugadores.get(0));
+                }
+                break;
+            }
+        }
     }
 }
--- app/src/main/java/com/example/retopmdruleta/PantallaConfiguracion.java
+++ app/src/main/java/com/example/retopmdruleta/PantallaConfiguracion.java
@@ -1,43 +1,96 @@
 package com.example.retopmdruleta;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.gridlayout.widget.GridLayout;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.TextView;
 
-public class PantallaConfiguracion extends AppCompatActivity implements  RadioGroup.OnCheckedChangeListener  {
+import java.util.ArrayList;
+
+public class PantallaConfiguracion extends AppCompatActivity {
 
 
-    RadioButton radioButton = null;
-    RadioButton radioButton2 = null;
-    RadioButton radioButton3 = null;
-    RadioButton radioButton4 = null;
-    RadioButton radioButton5 = null;
-    RadioButton radioButton6 = null;
-    RadioGroup radioGroup = null;
-    Button botonCancelar = null;
-    Button botonJugar = null;
+    private RadioButton radioButton2 = null;
+    private RadioButton radioButton3 = null;
+    private RadioButton radioButton4 = null;
+    private RadioButton radioButton5 = null;
+    private RadioButton radioButton6 = null;
+    private RadioGroup radioGroup = null;
+    private Button botonCancelar = null;
+    private Button botonJugar = null;
+    private ArrayList<String> jugadores = new ArrayList<>();
+
+    private GridLayout gL = null;
+    private String numeroJugadores = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pantalla_configuracion2);
-        radioButton = (RadioButton) findViewById(R.id.radioButton);
         radioButton2 = (RadioButton) findViewById(R.id.radioButton2);
         radioButton3 = (RadioButton) findViewById(R.id.radioButton3);
         radioButton4 = (RadioButton) findViewById(R.id.radioButton4);
         radioButton5 = (RadioButton) findViewById(R.id.radioButton5);
         radioButton6 = (RadioButton) findViewById(R.id.radioButton6);
-        radioGroup = (RadioGroup) findViewById(R.id.radioG);
-        radioGroup.setOnCheckedChangeListener(this);
+        radioGroup = (RadioGroup) findViewById(R.id.radioGroup);
+        botonJugar = (Button) findViewById(R.id.button);
+        gL = findViewById(R.id.gridLayout);
+        botonJugar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Return numero de jugadores y nombre de los jugadores
+                for (int i = 0; i < gL; i++) {
+                    String nombre = gL.;
+                    jugadores.add(nombre);
+                }
+                Intent returnIntent = new Intent(PantallaConfiguracion.this, MainActivity.class);
+                returnIntent.putExtra("numeroJugadores", numeroJugadores);
+                returnIntent.putExtra("jugadores", jugadores);
+                setResult(Activity.RESULT_OK, returnIntent);
+                finish();
+
+            }
+        });
+        botonCancelar = (Button) findViewById(R.id.button2);
+        botonCancelar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                RadioButton rb = (RadioButton) findViewById(group.getCheckedRadioButtonId());
+                gL.removeAllViews();
+                if (rb.isChecked()) {
+                    numeroJugadores = rb.getText().toString();
+                    crearJugadores();
+                }
+            }
+        });
     }
 
-    @Override
-    public void onCheckedChanged(RadioGroup group, int checkedId) {
-
+    private void crearJugadores() {
+        for (int i = 0; i < Integer.parseInt(numeroJugadores); i++) {
+            EditText eT = new EditText(this);
+            eT.setId(View.generateViewId());
+            eT.setLayoutParams(new ViewGroup.LayoutParams(
+                    ViewGroup.LayoutParams.WRAP_CONTENT,
+                    ViewGroup.LayoutParams.WRAP_CONTENT));
+            eT.setWidth(400);
+            eT.setHeight(150);
+            gL.addView(eT);
+        }
     }
 }
